3b9fe3015200a7b86e9381b7a2db7c5ee89f63fc
Varenya Sawant <varenyasawant23@gmail.com>
2025-02-12 11:16:20 +0530

    Update index.js of backend

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,22 @@
+const servicesRouter = require('./routes/services');
+const forumRoutes = require("./routes/forum");
+
+const PORT = 5000;
+
 const express = require('express');
 const app = express();
 
+const cors = require('cors');
+
 // Middleware
+app.use(cors());
 app.use(express.json());
 
-// Default route
-app.get('/', (req, res) => {
-    res.send('Welcome to the Gfest Backend!');
-});
+// Routes
+app.use('/services', servicesRouter);
 
-// Start the server
-const PORT = process.env.PORT || 5000;
+app.use("/api", forumRoutes);
+
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}`);
 });
